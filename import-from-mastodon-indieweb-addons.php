596e3f7f0690c1323a63b8c19ff1eb87c8d9f96d
Dinis Correia <dinis.correia@trojan-unicorn.com>
2022-11-22 14:38:26 +0000

    Add plugin file

---
+++ import-from-mastodon-indieweb-addons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Import from Mastodon IndieWeb Addions
+ * Description: Add IndieWeb improvements to Import From Mastodon WordPress plugin.
+ * Author:      Dinis Correia
+ * Author URI:  https://diniscorreia.com/
+ * License:     GNU General Public License v3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Version:     0.1
+ */
+
+
