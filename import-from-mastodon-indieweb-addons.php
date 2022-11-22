3e01309dab4598c781a6dccea8cb27fec9b4f3fe
Dinis Correia <dinis.correia@trojan-unicorn.com>
2022-11-22 14:53:00 +0000

    Clean up post content

--- import-from-mastodon-indieweb-addons.php
+++ import-from-mastodon-indieweb-addons.php
@@ -9,21 +9,51 @@
  * Version:     0.1
  */
 
+/**
+ * Cleans up post content
+ */
+add_filter( 'import_from_mastodon_post_content', function( $content, $status ) {
+    if ( ! empty( $status->favourited ) || ( isset( $status->reblog->url ) && isset( $status->reblog->account->username ) ) ) {
+        // Opionated choice, but for favourites and boost we don't want
+        // to store a copy of the content, just a likn to it.
+        $content = '';
+    } elseif ( isset( $status->in_reply_to_id ) ) {
+        // For replies, context about the parent tweak will be stored in
+        // the post meta, so no need to duplicate it in the content.
+        $content = trim(
+            wp_kses(
+                $status->content,
+                array(
+                    'a'  => array(
+                        'href' => array(),
+                    ),
+                    'br' => array(),
+                    'p'  => array(),
+                )
+            )
+        );
+  
+        $content = preg_replace('/^(<p><a href="\S+">@)[a-zA-Z0-9_]+?(<\/a> )/', '<p>', $content);
+        $content = trim( $content );
+    }
+  
+    return $content;
+}, 1, 2 );
 
 /**
  * Adds Mastodon URL to syndication links
  */
 add_filter( 'syn_add_links', function( $urls, $object_id ) {
     if ( ! empty( $get_post_meta( $object_id, 'mf2_like-of' ) ) || ! empty( $get_post_meta( $object_id, 'mf2_repost-of' ) ) ) {
-      // Favourites and boosts shouldn't get syndication links.
-      return;
+        // Favourites and boosts shouldn't get syndication links.
+        return;
     }
 
     $mastodon_url = get_post_meta( $object_id, '_import_from_mastodon_url', true );
   
     if ( ! empty( $mastodon_url ) ) {
-      $urls[] = $mastodon_url;
+        $urls[] = $mastodon_url;
     }
   
     return $urls;
-  }, 1, 2 );
+}, 1, 2 );
